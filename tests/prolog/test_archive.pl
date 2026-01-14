1a295af56c2dc4e984e94d062e701f8376714014
Jameson <555@jamesontravers.dev>
2026-01-14 11:05:54 -0500

    test(archive): add place and sample_elite tests

--- tests/prolog/test_archive.pl
+++ tests/prolog/test_archive.pl
@@ -12,4 +12,35 @@ test(state_archive_returns_empty_for_new_round) :-
     state_archive(0, S, Archive),
     assoc_to_list(Archive, []).
 
+test(place_single_warrior) :-
+    empty_state(S0),
+    place(0, bc(1,2), warrior_1, 0.5, S0, S1),
+    get_best(0, S1, Champion),
+    Champion = warrior_1.
+
+test(place_replaces_inferior) :-
+    empty_state(S0),
+    place(0, bc(1,2), warrior_1, 0.5, S0, S1),
+    place(0, bc(1,2), warrior_2, 0.7, S1, S2),
+    get_best(0, S2, Champion),
+    Champion = warrior_2.
+
+test(place_keeps_superior) :-
+    empty_state(S0),
+    place(0, bc(1,2), warrior_1, 0.8, S0, S1),
+    place(0, bc(1,2), warrior_2, 0.6, S1, S2),
+    get_best(0, S2, Champion),
+    Champion = warrior_1.
+
+test(sample_elite_fails_on_empty) :-
+    empty_state(S),
+    \+ sample_elite(0, S, _).
+
+test(sample_elite_returns_member) :-
+    empty_state(S0),
+    place(0, bc(1,1), w1, 0.5, S0, S1),
+    place(0, bc(2,2), w2, 0.6, S1, S2),
+    sample_elite(0, S2, Elite),
+    memberchk(Elite, [w1, w2]).
+
 :- end_tests(archive).
